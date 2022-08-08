5e57cacbbdcc63b8d363b878b886f7e7800fe3bc
Helen Jiang <106964699+Jiang-haihong@users.noreply.github.com>
2022-08-08 17:04:23 +1000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCrhTblNHuCVn4s5QWtLWYO0jSP3jDXvAs82b48yMW9e2FrekkMyq52DMSgqJcVbqCCpG8NOFpEmfqz/Wv/aiHkcqZcgT3azq+ck4uZxavUHbtAshSD0NECWJsuAEfIr3PV9CuHkYz+xtgqtnzJoq9Mlzqe8l+QYxT5+eAeBxodMELoFoKMnSgf0Q0kJh0ig+5gQB4bvEDmlvigXWjkRVno0LFcWGhzbguL634x+mK0D06okxuDozpY5EdUw9BnaV5fP5A+BGa4VSBWMqGLTabC5QIXiY7Xa+MRX1JhJhU++F65uq2lC0LrmmZtzXmY2IPm9SRB/BXU5aXeYIMuueB7',
 	}  
 }
