499c90b3f216be212c436edaa2fd11fffca0f8ca
Helen Jiang <106964699+Jiang-haihong@users.noreply.github.com>
2022-08-15 17:19:27 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -20,4 +20,9 @@ class minecraft {
         ensure => file,
         source => 'puppet:///modules/minecraft/minecraft.service',
         }
+        
+        service {'minecraft':
+          ensure => running,
+          enable => true,
+          }
     }
