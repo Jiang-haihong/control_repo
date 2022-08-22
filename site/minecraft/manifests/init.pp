8e3ff0d974efdb4e66ac2eaa8c24f86e73a73c10
Helen Jiang <106964699+Jiang-haihong@users.noreply.github.com>
2022-08-22 12:34:09 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -26,7 +26,6 @@ class minecraft (
         ##source => 'puppet:///modules/minecraft/minecraft.service',
         content  => epp('minecraft/minecraft.service',{
         install_dir => $install_dir,}),
-        ,
         }
         
        service {'minecraft':
